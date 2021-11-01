dd4ae385dde35aeef4b87381fdd56e2387cf1e13
Alexander Brassel <brasselsprouts@fb.com>
2021-11-01 16:55:42 -0700

    project skel for uniq

--- uniq/src/main.rs
+++ uniq/src/main.rs
@@ -1,3 +1,30 @@
+use structopt::StructOpt;
+
+#[allow(unused)]
+#[derive(StructOpt)]
+/// The `uniq` utility provides facilities for evaluating uniqueness of lines.
+struct Opt {
+    #[structopt(short, long, help="Precede each line with the number of times it occured in the input, followed by a space")]
+    count: bool,
+    #[structopt(short, long, conflicts_with = "unique", help = "Only output repeated lines")]
+    repeated: bool,
+    #[structopt(short, long = "skip-words", help = "Ignore the first <words> words. This is one based.")]
+    words: Option<usize>,
+    #[structopt(
+        short,
+        long = "skip-graphemes",
+        help = "Ignore the first <graphemes> graphemes. This is one based.
+                If used in conjunction with \"-d\", applies second."
+    )]
+    graphemes: Option<usize>,
+    #[structopt(short, long, help = "Perform case insensitive line comparisons.")]
+    insensitive: bool,
+    #[structopt(short, long, help = "Only output lines that are not repeated in the input.")]
+    unique: bool,
+    #[structopt(long, help = "Consider non-adjacent lines for uniqueness")]
+    non_adjacent: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = Opt::from_args();
 }
