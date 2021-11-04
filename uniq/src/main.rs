96a01016d7164780675ab9fdb23d22ac1c8dfcdc
Alexander Brassel <brasselsprouts@fb.com>
2021-11-04 09:50:48 -0700

    Fixed formatting and pinned a rust version for the project

--- uniq/src/main.rs
+++ uniq/src/main.rs
@@ -6,11 +6,25 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 /// The `uniq` utility provides facilities for evaluating uniqueness of lines.
 struct Opt {
-    #[structopt(short, long, help="Precede each line with the number of times it occured in the input, followed by a space")]
+    #[structopt(
+        short,
+        long,
+        help = "Precede each line with the number of times it occured in the input, followed by a \
+                space"
+    )]
     count: bool,
-    #[structopt(short, long, conflicts_with = "unique", help = "Only output repeated lines")]
+    #[structopt(
+        short,
+        long,
+        conflicts_with = "unique",
+        help = "Only output repeated lines"
+    )]
     repeated: bool,
-    #[structopt(short, long = "skip-words", help = "Ignore the first <words> words. This is one based.")]
+    #[structopt(
+        short,
+        long = "skip-words",
+        help = "Ignore the first <words> words. This is one based."
+    )]
     words: Option<usize>,
     #[structopt(
         short,
@@ -21,11 +35,17 @@ struct Opt {
     graphemes: Option<usize>,
     #[structopt(short, long, help = "Perform case insensitive line comparisons.")]
     insensitive: bool,
-    #[structopt(short, long, help = "Only output lines that are not repeated in the input.")]
+    #[structopt(
+        short,
+        long,
+        help = "Only output lines that are not repeated in the input."
+    )]
     unique: bool,
     #[structopt(long, help = "Consider non-adjacent lines for uniqueness")]
     non_adjacent: bool,
-    #[structopt(help = "Input file - defaults to `stdin` if not provided, or if filename is \"-\"")]
+    #[structopt(
+        help = "Input file - defaults to `stdin` if not provided, or if filename is \"-\""
+    )]
     input: Option<PathBuf>,
     #[structopt(help = "Output file - defaults to `stdout` if not provided")]
     output: Option<PathBuf>,
