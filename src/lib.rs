bb97481b01dafe100a20a79112aa39b4a090f0e0
Alexander Brassel <abrassel@umd.edu>
2021-11-05 08:57:53 -0700

    formatted

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,23 @@
 
 #![feature(box_syntax)]
 
-use std::{fs::{File, OpenOptions}, io::{self, Read, Write}, path::{Path, PathBuf}};
+use std::{
+    fs::{File, OpenOptions},
+    io::{self, Read, Write},
+    path::{Path, PathBuf},
+};
 
-pub fn convert_streams(input: Option<PathBuf>, output: Option<PathBuf>) -> anyhow::Result<(Box<dyn Read>, Box<dyn Write>)> {
+pub fn convert_streams(
+    input: Option<PathBuf>,
+    output: Option<PathBuf>,
+) -> anyhow::Result<(Box<dyn Read>, Box<dyn Write>)> {
     let input = match input {
         Some(input) if input != Path::new("-") => box File::open(input)? as Box<dyn Read>,
         _ => box io::stdin() as Box<dyn Read>,
     };
     let output = match output {
         None => box io::stdout() as Box<dyn Write>,
-        // important not to truncate what is already in the file.   
+        // important not to truncate what is already in the file.
         Some(output) => box OpenOptions::new().write(true).open(output)? as Box<dyn Write>,
     };
 
