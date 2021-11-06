fe9e2ca570d3b713c89ff1871eae459726371bb5
Alexander Brassel <awildbrasselsprouts@gmail.com>
2021-11-06 11:14:33 -0700

    fixed type signatures

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,19 +1,19 @@
 use std::{
     fs::{File, OpenOptions},
     io::{self, Read, Write},
-    path::{Path, PathBuf},
+    path::Path,
 };
 
 #[allow(unused)]
-pub fn convert_input(input: Option<PathBuf>) -> io::Result<Box<dyn Read>> {
+pub fn convert_input<P: AsRef<Path>>(input: Option<P>) -> io::Result<Box<dyn Read>> {
     Ok(match input {
-        Some(input) if input != Path::new("-") => box File::open(input)?,
+        Some(input) if input.as_ref() != Path::new("-") => box File::open(input)?,
         _ => box io::stdin(),
     })
 }
 
 #[allow(unused)]
-pub fn convert_output(output: Option<PathBuf>) -> io::Result<Box<dyn Write>> {
+pub fn convert_output<P: AsRef<Path>>(output: Option<P>) -> io::Result<Box<dyn Write>> {
     Ok(match output {
         None => box io::stdout(),
         Some(output) => box OpenOptions::new().write(true).open(output)?,
