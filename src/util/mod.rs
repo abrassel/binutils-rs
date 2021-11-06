fb4bd202330800dbc910e2e05b6e72d215362608
Alexander Brassel <awildbrasselsprouts@gmail.com>
2021-11-06 10:56:23 -0700

    added utility + command libs

---
+++ src/util/mod.rs
@@ -0,0 +1,21 @@
+use std::{
+    fs::{File, OpenOptions},
+    io::{self, Read, Write},
+    path::{Path, PathBuf},
+};
+
+#[allow(unused)]
+pub fn convert_stdin(input: Option<PathBuf>) -> io::Result<Box<dyn Read>> {
+    Ok(match input {
+        Some(input) if input != Path::new("-") => box File::open(input)?,
+        _ => box io::stdin(),
+    })
+}
+
+#[allow(unused)]
+pub fn convert_stdout(output: Option<PathBuf>) -> io::Result<Box<dyn Write>> {
+    Ok(match output {
+        None => box io::stdout(),
+        Some(output) => box OpenOptions::new().write(true).open(output)?,
+    })
+}
